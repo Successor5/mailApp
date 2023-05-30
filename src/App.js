cbf22052b8a338af89bde2cb901d0e7a1d19caff
Debbie <119403063+debby71711@users.noreply.github.com>
2023-05-30 11:23:09 +0100

    q

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import DeleteIcon from './MyAssets/Image/delete.svg';
+import DraftIcon from './MyAssets/Image/draft.svg';
+import ArchiveIcon from './MyAssets/Image/Archive.svg';
 import './App.css';
 import Email from './Pages/DashBoard/Components/emails';
 function App() {
   return (
     <div className="App">
       <Email/>
+      <img className='draft' src={DraftIcon} alt="Draft Icon" />
+      <img className='archive' src={ArchiveIcon} alt="Archive Icon" />
+      <img className='delete' src={DeleteIcon} alt="Delete Icon" />
+
     </div>
   );
 }
--- src/Pages/DashBoard/Components/emails.jsx
+++ src/Pages/DashBoard/Components/emails.jsx
@@ -1,26 +1,67 @@
 import React from 'react';
 import '../Styles/Email.css';
+import { useState } from 'react';
 function Email(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
     return(
         <div className='main'>
-             <div className='me'>
-                {/* <button className='circle1'>
+            <div className='firstBox'>
+            <div className='me'>
+                 <button className='circle1'>
                 </button>
                 <div className='circle2'>
                 </div>
                 <div className='circle3'>
-                </div> */}
-            </div> 
-            <div className='firstBox'>
-                <button className='buy'>Gmail</button> 
+                </div> 
+                </div> 
+            <div className='none'>
+            <div>
+            <button className='buy'onClick={toggleDropdown }>Gmail</button> 
+      {isOpen && (
+        <div>
+                <div className='inb'>
+                    <h4 className='test'>Inbox</h4>
+                </div>    
+                <div className='sent1' placeholder='hhh'>
+                    <h4 className='test'>Sent</h4>
+                </div>          
+                <div className='Starred'>
+                    <h5 className='test'>Starred</h5>
+                </div>
+                <div className='Archive'>
+                    <h5 className='test'>Archive</h5>
+                </div>
+                <div className='Delete'>
+                    <h5 className='test'>🚮Delete</h5>
+                </div>
+                <div className='Draft'>
+                    <h5 className='test'>Draft</h5>
+            </div>        
+            </div>
+      )}
+    </div>
+    </div>
+            <div className='down'>
+                <h3 className='j'>Folder</h3>
+                <h3 className='j2'> 📂Finance</h3>
+                </div>
             </div>
             <div className='secoundBox'>
                 <h1 className='inbox'>Inbox</h1>
-            </div>
+            <input className='dot' type="text" placeholder="🔍Search..." />
+      <button className='dot2'>Search▯</button>
+      <div className='down2'>
+      </div>
+      </div>
             <div className='thirdBox'>
-                3
+                <div className='down3'>
+                </div>
             </div>
         </div>
-    )
+  )
 }
 export default Email;
